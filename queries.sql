e1d4a293aa537ae5df32f949745a939edaec1852
Misigno <efournier92@gmail.com>
2016-05-12 18:56:29 -0400

    Initial Commit

---
+++ queries.sql
@@ -0,0 +1,21 @@
+-- Find all rows that have an ingredient name of Brussels sprouts.
+SELECT * FROM ingredients
+  WHERE name = 'Brussels sprouts';
+
+-- Calculate the total count of rows of ingredients with a name of Brussels sprouts.
+SELECT COUNT(*) FROM ingredients
+  WHERE name = 'Brussels sprouts';
+
+-- Find all Brussels sprouts ingredients having a unit type of gallon(s).
+SELECT * FROM ingredients
+  WHERE name = 'Brussels sprouts' AND unit = 'gallon(s)';
+
+-- Find all rows that have a unit type of gallon(s), a name of Brussels sprouts or has the letter j in it.
+  SELECT * FROM ingredients
+  WHERE name = 'Brussels sprouts' OR unit = 'gallon(s)' OR name LIKE '%j%';
+
+-- Create indices to speed up quering
+  CREATE INDEX ON ingredients(name);
+  CREATE INDEX ON ingredients(unit);
+
+-- EXPLAIN ANALYZE
